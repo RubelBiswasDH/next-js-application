424441873d2fe7464d1420e84130ae3cd165ed92
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-07-20 22:59:48 +0600

    Initiated project

---
+++ pages/registration.tsx
@@ -0,0 +1,7 @@
+export default function Registration() {
+  return (
+    <div className={ `bg-cover bg-center min-h-screen flex flex-row items-center justify-center overflow-auto p-16 bg-[url('/images/login-bg.jpg')]` }>
+        { 'Registration' }
+    </div>
+  )
+}
